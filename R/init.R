c316fdad7cf6f43a3ac3ed267a34cdb0948334f2
kisungyou <ksyou496@gmail.com>
2019-05-09 23:11:02 -0400

    'init' function added.

---
+++ R/init.R
@@ -0,0 +1,21 @@
+.pkgenv <- new.env(parent = emptyenv())
+
+.onAttach <- function(...){
+  ## Retrieve Year Information
+  date <- date()
+  x <- regexpr("[0-9]{4}", date)
+  this.year <- substr(date, x[1], x[1] + attr(x, "match.length") - 1)
+
+  # Retrieve Current Version
+  this.version = packageVersion("graphon")
+
+  ## Print on Screen
+  packageStartupMessage("----------------------------------------------------------")
+  packageStartupMessage("**   graphon - A Collection of Graphon Estimation Methods")
+  packageStartupMessage("**")
+  packageStartupMessage("** Version    : ",this.version,"      (",this.year,")",sep="")
+  packageStartupMessage("** Maintainer : Kisung You (ksyou496@gmail.com)")
+  packageStartupMessage("**")
+  packageStartupMessage("** Please share any bugs or suggestions to the maintainer.")
+  packageStartupMessage("----------------------------------------------------------")
+}
